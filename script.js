231f1f6f7720ea25651648cd08c1d7388db575bf
Badboy295-crazy <amberkasaudhan37@gmail.com>
2025-06-23 16:57:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+//Prevent right click
+document.oncontextmenu = () =>  {
+    alert("Don't try to right click")
+    return false
+}
+
+
+//Still anyonecan inspect element by F12 key. View Page source by 
+// Ctrl + U key. Copy by Ctrl + C key.  Paste by Ctrl + V key. Let's prevent this
+
+document.onkeydown = e => {
+    //Prevent F12 key
+    if(e.key == "F12") {
+        alert("Don't try to inspect element")
+            return false
+    } 
+
+    //Prevent showing page source by Ctrl + u
+    if(e.ctrlKey && e.key == "u"){
+         alert("Don't try to view page source")
+            return false
+    } 
+    
+    //Prevent copying anything from the page 
+    if(e.ctrlKey && e.key == "c"){
+         alert("Don't try to copy page element")
+            return false
+    } 
+
+    //Prevent paste anything from other sources
+    if(e.ctrlKey && e.key == "v"){
+        alert("Don't try to paste anything to page")
+            return false
+    }
+
+    //Prevent from save
+     if(e.ctrlKey && e.key == "s"){
+         alert("Don't try to save page element")
+            return false
+    } 
+
+
+}
